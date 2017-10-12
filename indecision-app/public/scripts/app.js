6edb6bf124ad78b22945d089ed773e87060b3f4f
jonkur <jkuronen11@gmail.com>
2017-10-12 11:46:48 +0300

    moved forward to components

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,40 +1,128 @@
 'use strict';
 
-console.log('Build it app started!');
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
 
-var msgVisible = false;
-var toggleMsg = function toggleMsg() {
-    msgVisible = !msgVisible;
-    render();
-};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var appRoot = document.getElementById('app');
-var render = function render() {
-    var template = React.createElement(
-        'div',
-        null,
-        React.createElement(
-            'h1',
-            null,
-            'Visibility Toggle App'
-        ),
-        React.createElement(
-            'p',
-            null,
-            'Click on the button below to show or hide the message!'
-        ),
-        React.createElement(
-            'button',
-            { onClick: toggleMsg },
-            msgVisible ? 'Hide Secret' : 'Show Secret'
-        ),
-        msgVisible && React.createElement(
-            'p',
-            null,
-            'Psst... It\'s time to call it a day.'
-        )
-    );
-    ReactDOM.render(template, appRoot);
-};
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-render();
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Person = function () {
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+        var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+        var gender = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'male';
+
+        _classCallCheck(this, Person);
+
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            return 'Hi. I am ' + this.name + '!';
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            return 'This is ' + this.name + '. ' + (this.gender === 'male' ? 'He' : 'She') + ' is ' + this.age + ' years old.';
+        }
+    }]);
+
+    return Person;
+}();
+
+var Student = function (_Person) {
+    _inherits(Student, _Person);
+
+    function Student(name, age, gender, major) {
+        _classCallCheck(this, Student);
+
+        var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age, gender));
+
+        _this.major = major;
+        return _this;
+    }
+
+    _createClass(Student, [{
+        key: 'hasMajor',
+        value: function hasMajor() {
+            return !!this.major;
+        }
+    }, {
+        key: 'getDescription',
+        value: function getDescription() {
+            var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
+            if (this.hasMajor()) {
+                description += ' ' + (this.gender === 'male' ? 'His' : 'Her') + ' major is ' + this.major + '.';
+            }
+
+            return description;
+        }
+    }]);
+
+    return Student;
+}(Person);
+
+var Traveler = function (_Person2) {
+    _inherits(Traveler, _Person2);
+
+    function Traveler(name, age, gender, homeLocation, placesVisited) {
+        _classCallCheck(this, Traveler);
+
+        var _this2 = _possibleConstructorReturn(this, (Traveler.__proto__ || Object.getPrototypeOf(Traveler)).call(this, name, age, gender));
+
+        _this2.homeLocation = homeLocation;
+        _this2.placesVisited = placesVisited;
+        return _this2;
+    }
+
+    _createClass(Traveler, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            var _this3 = this;
+
+            var greeting = _get(Traveler.prototype.__proto__ || Object.getPrototypeOf(Traveler.prototype), 'getGreeting', this).call(this);
+            if (this.homeLocation) {
+                greeting += ' I\'m from ' + this.homeLocation + '.';
+            }
+            if (this.placesVisited.length > 0) {
+                var visitedStr = ' I have been to';
+                var placesStr = this.placesVisited.reduce(function (last, now, i) {
+                    return last += ' ' + now + (i === _this3.placesVisited.length - 1 ? '.' : ',');
+                });
+                greeting += visitedStr + ' ' + placesStr;
+            }
+
+            return greeting;
+        }
+    }]);
+
+    return Traveler;
+}(Person);
+
+var me = new Student('Jonas', 29, 'male', 'Computer Science');
+console.log(me);
+console.log(me.getGreeting());
+console.log(me.getDescription());
+console.log('Has major: ' + me.hasMajor());
+
+var jen = new Student('Jen', 26, 'female');
+console.log(jen);
+console.log(jen.getDescription());
+console.log('Has major: ' + jen.hasMajor());
+
+var other = new Student();
+console.log(other.getGreeting());
+
+console.log('\n--------------------------------------\n');
+
+var heebo = new Traveler('Heebo', 35, 'male', 'Helsinki', ['Stockholm', 'London', 'Johannesburg']);
+console.log(heebo);
+console.log(heebo.getGreeting());
---
+++ indecision-app/src/playground/es6-classes-1.js
@@ -0,0 +1,80 @@
+
+class Person {
+    constructor(name = 'Anonymous', age = 0, gender = 'male') {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    getGreeting() {
+        return `Hi. I am ${this.name}!`;
+    }
+
+    getDescription() {
+        return `This is ${this.name}. ${this.gender === 'male' ? 'He' : 'She'} is ${this.age} years old.`;
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, gender, major) {
+        super(name, age, gender);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += ` ${this.gender === 'male' ? 'His' : 'Her'} major is ${this.major}.`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, gender, homeLocation, placesVisited) {
+        super(name, age, gender);
+        this.homeLocation = homeLocation;
+        this.placesVisited = placesVisited;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if (this.homeLocation) {
+            greeting += ` I'm from ${this.homeLocation}.`;
+        }
+        if (this.placesVisited.length > 0) {
+            let visitedStr = ' I have been to';
+            let placesStr = this.placesVisited.reduce((last, now , i) => {
+                return last += ` ${now}${i === this.placesVisited.length - 1 ? '.' : ','}`;
+            });
+            greeting += `${visitedStr} ${placesStr}`;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Student('Jonas', 29, 'male', 'Computer Science');
+console.log(me)
+console.log(me.getGreeting());
+console.log(me.getDescription());
+console.log(`Has major: ${me.hasMajor()}`);
+
+const jen = new Student('Jen', 26, 'female');
+console.log(jen);
+console.log(jen.getDescription());
+console.log(`Has major: ${jen.hasMajor()}`);
+
+const other = new Student();
+console.log(other.getGreeting());
+
+console.log('\n--------------------------------------\n');
+
+const heebo = new Traveler('Heebo', 35, 'male', 'Helsinki', ['Stockholm', 'London', 'Johannesburg']);
+console.log(heebo);
+console.log(heebo.getGreeting());
