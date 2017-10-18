c437a0e63151dbed7b24930f06f05f6f28185f26
jonkur <jkuronen11@gmail.com>
2017-10-18 15:50:36 +0300

    finished Indecision App, now starting on Part 2.

--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -69,15 +69,17 @@ export default class IndecisionApp extends React.Component {
                 <Header subtitle={subtitle} />
                 <div className="container">
                     <Action hasOptions={this.state.options.length > 0} handlePick={this.handlePick} />
-                    <Options 
-                        options={this.state.options}
-                        handleDeleteOptions={this.handleDeleteOptions}
-                        handleDeleteOption={this.handleDeleteOption} 
-                    />
-                    <AddOption 
-                        handleAddOption={this.handleAddOption}
-                    />
-                    <OptionModal 
+                    <div className="widget">
+                        <Options
+                            options={this.state.options}
+                            handleDeleteOptions={this.handleDeleteOptions}
+                            handleDeleteOption={this.handleDeleteOption} 
+                        />
+                        <AddOption
+                            handleAddOption={this.handleAddOption}
+                        />
+                    </div>
+                    <OptionModal
                         selectedOption={this.state.selectedOption}
                         handleClearSelectedOption={this.handleClearSelectedOption}
                     />
--- indecision-app/src/components/OptionModal.js
+++ indecision-app/src/components/OptionModal.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-const OptionModal = (props) => (
-    <Modal
-        isOpen={!!props.selectedOption}
-        onRequestClose={props.handleClearSelectedOption}
-        contentLabel="Selected Option"
-    >
-        <h3>Selected Option</h3>
-        { props.selectedOption && <p>{props.selectedOption}</p> }
-        <button onClick={props.handleClearSelectedOption}>Okay</button>
-    </Modal>
-);
+export default class OptionModal extends React.Component {
+    state = {
+        isOpen: false,
+        selectedOption: undefined
+    }
 
-export default OptionModal;
+    componentDidUpdate() {
+        if (this.props.selectedOption !== this.state.selectedOption) {
+            this.setState({
+                isOpen: !!this.props.selectedOption,
+                selectedOption: this.props.selectedOption
+            });
+        }
+    }
+
+    handleClearSelectedOption = () => {
+        this.setState({isOpen: false});
+        // Wait until the fadeout animation has time to finish,
+        // as parent handleClearSelectedOption will clear the
+        // selectedOption here as well
+        setTimeout(() => {
+            this.props.handleClearSelectedOption();
+        }, 200);
+    };
+
+    render() {
+        return (
+            <Modal
+                isOpen={this.state.isOpen}
+                onRequestClose={this.handleClearSelectedOption}
+                contentLabel="Selected Option"
+                closeTimeoutMS={200}
+                className="modal"
+            >
+                <h3 className="modal__title">Selected Option</h3>
+                {this.state.selectedOption && <p className="modal__body">{this.state.selectedOption}</p>}
+                <button className="button" onClick={this.handleClearSelectedOption}>Okay</button>
+            </Modal>
+        );
+    }
+}
