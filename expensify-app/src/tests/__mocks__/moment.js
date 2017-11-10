f5ebe9eb6a38b7335edb83ed9e2290718ac0357c
jonkur <jkuronen11@gmail.com>
2017-11-10 16:50:15 +0200

    removed expensify app from this repo

--- expensify-app/src/tests/__mocks__/moment.js
+++
@@ -1,5 +0,0 @@
-const moment = require.requireActual('moment'); // normal import would not work, would call this moment mock file. This is why requireActual is used.
-
-export default (timestamp = 0) => {
-  return moment(timestamp);
-};
